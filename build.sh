addd9db5a40a8e7ccf8472db5ce693aabe8268bd
Nisal Bandara <nisal.bandara@outlook.com>
2022-07-11 21:27:20 +0530

    Rename the project to 'sam'

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env sh
 
-# build `example` script from `example.sh`
+# build `sam` script from `sam.sh`
 # basically insert from files `files/' dir into the script
 
 echo "Removing existing build..."
-rm -fv example
+rm -fv sam
 
-echo "Building 'example'"
-cp example.sh example
-chmod +x example
+echo "Building 'sam'"
+cp sam.sh sam
+chmod +x sam
 
-for file in `grep '##<!' example.sh | sed -E 's/##<! \((.*)\)/\1/'`
+for file in `grep '##<!' sam.sh | sed -E 's/##<! \((.*)\)/\1/'`
 do
     if [ -r $file ]
     then
         cp $file $file.esc
         sed -i -e 's/\$/\\$/g' $file.esc
-        sed -i -e "/##<! (${file/\//\\/})/r $file.esc" -e "/##<! (${file/\//\\/})/d" example
+        sed -i -e "/##<! (${file/\//\\/})/r $file.esc" -e "/##<! (${file/\//\\/})/d" sam
         rm $file.esc
     else
         echo "$file is not readable"
